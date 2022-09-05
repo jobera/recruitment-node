c4db8affb15095652d2e1e6584a047c6711cf0c9
jobera <jobera.dev@hotmail.com>
2022-09-05 12:02:37 +0100

    Remove non used files for app module

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CertificatesModule } from './certificates/certificates.module';
 import { DatabaseProviderModule } from './providers/database/provider.module';
 
 @Module({
   imports: [DatabaseProviderModule, AuthModule, CertificatesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
